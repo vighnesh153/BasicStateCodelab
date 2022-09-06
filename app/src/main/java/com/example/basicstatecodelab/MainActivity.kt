6205a43e054d12617fcd344dc75d50045b2c6544
Vighnesh Raut <vighnesh.raut13@gmail.com>
2022-09-06 15:32:41 +0530

    initial commit

---
+++ app/src/main/java/com/example/basicstatecodelab/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.basicstatecodelab
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.basicstatecodelab.ui.theme.BasicStateCodelabTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContent {
+      App()
+    }
+  }
+}
+
+@Composable
+fun App() {
+  BasicStateCodelabTheme {
+    // A surface container using the 'background' color from the theme
+    Surface(
+      modifier = Modifier.fillMaxSize(),
+      color = MaterialTheme.colors.background
+    ) {
+      WellnessScreen()
+    }
+  }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppPreview() {
+  App()
+}
